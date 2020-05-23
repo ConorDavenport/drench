e14d608a6aa76d7955bb23d7431935be8966fc77
ConorDavenport <cnrdavenport@gmail.com>
2020-05-23 13:18:55 +0100

    add grid initialisation

--- src/drench.cpp
+++ src/drench.cpp
@@ -9,7 +9,7 @@ using namespace std;
 #define GRID 14
 
 class Node {
-  private:
+  public:
     // 0 green
     // 1 pink
     // 2 purple
@@ -23,8 +23,6 @@ class Node {
     int id;
     // connections to other tiles
     vector<Node*> connections;
-
-  public:
 };
 
 int rng() {
@@ -38,8 +36,12 @@ int main() {
   // represents the grid of tiles
   Node grid[GRID][GRID] = {};
 
+  // initialise grid tiles with uuid's and random colours
   for (int i = 0; i < GRID; i ++) {
-    
+    for (int j = 0; j < GRID; j++) {
+      grid[i][j].id = ((i * GRID) + j);
+      grid[i][j].colour = rng();
+    }
   }
   
   
