8018a911a39106da8593438ddb4d1b4ccac652d3
ConorDavenport <cnrdavenport@gmail.com>
2021-04-08 10:06:14 +0100

    fix quickSolve func

--- src/drench.cpp
+++ src/drench.cpp
@@ -7,11 +7,12 @@
 #include <algorithm>
 #include "../lib/node.h"
 #include "../lib/comparisons.h"
+#include "../lib/colours.h"
 
 using namespace std;
 
 #define GRID 14
-
+#define NUM_COLOURS 6
 // get drench example game
 // load game into grid, return 2d array
 Node** parseData(string fileName) {
@@ -232,20 +233,38 @@ void quickSolve(vector<Node*>& network) {
   }
   
   Node* player = network[0];
-  sort(player->connections.begin(), player->connections.end(), compColour);
-  Node* nextMove = player->connections[0];
-  player->colour = nextMove->colour;
-  printf("%i\n", nextMove->colour);
 
+  colour colours[NUM_COLOURS];
+
+  // sort player connections by frequency of colour
+  for (int i = 0; i < NUM_COLOURS; i++) {
+    colours[i].colour = i;
+    colours[i].frequency = count_if(player->connections.begin(), player->connections.end(), [i](const Node* n){return n->colour == i;});
+  }
+
+  int len = sizeof(colours) / sizeof(colours[0]);
+  sort(colours, colours + len, compColourStructFrequency);
+
+  // choose next move as the most frequent colour
+  int nextMoveColour = colours[0].colour;
+  player->colour = nextMoveColour;
+  printf("%i ", nextMoveColour);
+
+  // select all adjacent nodes that colour is nextMoveColour
+  // append their connections to player.connections
   vector<Node*> nextMovesVec;
   vector<Node*> newConnections;
   for(vector<Node*>::iterator i = player->connections.begin(); i != player->connections.end(); i++) {
-    if((*i)->colour == nextMove->colour) {
+    if((*i)->colour == nextMoveColour) {
       nextMovesVec.push_back(*i);
       newConnections.insert(newConnections.end(), (*i)->connections.begin(), (*i)->connections.end());
     }
   }
-  player->connections = newConnections;
+  // remove duplicates and references to player in player.connections
+  player->connections.insert(player->connections.end(), newConnections.begin(), newConnections.end());
+  sort(player->connections.begin(), player->connections.end(), compId);
+  player->connections.erase(unique(player->connections.begin(), player->connections.end()), player->connections.end());
+  player->connections.erase(remove(player->connections.begin(), player->connections.end(), player), player->connections.end());
 
   // update all nodes that point to any node in nextMovesVec to point to player
   for(vector<Node*>::iterator i = network.begin(); i != network.end(); i++) {
@@ -258,6 +277,7 @@ void quickSolve(vector<Node*>& network) {
   
   player->connections.erase(find(player->connections.begin(), player->connections.end(), player));
 
+  // remove all adjacent nodes with colour nextMoveColour from network
   for(vector<Node*>::iterator i = nextMovesVec.begin(); i != nextMovesVec.end(); i++) {
     network.erase(remove(network.begin(), network.end(), *i), network.end());
   }
